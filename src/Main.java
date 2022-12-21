8b0e08dc77704982f59e2e6915f64ac10b1ed27d
acer <bekbolsunnasipbekov01@gmail.com>
2022-12-21 16:44:36 +0600

    nomer1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Animal animal0 = new Cat("Alicie", "Milk", 2, 1);
-        Animal animal1 = new Cow("Ferdinand", "Trava", 7, 148);
-        Animal animal2 = new Dog("Starkya", "Bones", 3, 2);
-        System.out.println(animal0 + "\n" + animal1 + "\n" + animal2);
-
-        Run run1=new Run() {
-            @Override
-            public void methods() {
-                System.out.println("drink milk");
-            }
-        };run1.methods();
-        Run run2=new Run() {
-            @Override
-            public void methods() {
-                System.out.println("eat grass");
-            }
-        };run2.methods();
-        Run run3=new Run() {
-            @Override
-            public void methods() {
-                System.out.println("eat meat");
-            }
-        } ;run3.methods();
+//        Animal animal0 = new Cat("Alicie", "Milk", 2, 1);
+//        Animal animal1 = new Cow("Ferdinand", "Trava", 7, 148);
+//        Animal animal2 = new Dog("Starkya", "Bones", 3, 2);
+//        System.out.println(animal0 + "\n" + animal1 + "\n" + animal2);
+//
+//        Run run1=new Run() {
+//            @Override
+//            public void methods() {
+//                System.out.println("drink milk");
+//            }
+//        };run1.methods();
+//        Run run2=new Run() {
+//            @Override
+//            public void methods() {
+//                System.out.println("eat grass");
+//            }
+//        };run2.methods();
+//        Run run3=new Run() {
+//            @Override
+//            public void methods() {
+//                System.out.println("eat meat");
+//            }
+//        } ;run3.methods();
+        //my github password is: bekbolsun07@
     }
 }
--- src/Run.java
+++ src/Run.java
@@ -1,3 +1,12 @@
-public abstract  class Run {
-    public abstract  void methods();
-}
+//public abstract  class Run {
+//    public abstract  void methods();
+//}
+public class Main (String [] args ){
+public static void main(String[]args){
+    Animal animal = new Cat ("")
+        }
+        }
+
+
+
+
